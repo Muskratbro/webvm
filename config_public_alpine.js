93ba58038b3cee3834d4cc467ebf344cb99c262b
Linus <younglin90@tcapsstudent.net>
2026-02-22 09:55:31 -0500

    Update config_public_alpine.js

--- config_public_alpine.js
+++ config_public_alpine.js
@@ -1,15 +1,28 @@
 // The root filesystem location
 export const diskImageUrl = "wss://disks.webvm.io/alpine_20251007.ext2";
+
 // The root filesystem backend type
 export const diskImageType = "cloud";
+
 // Print an introduction message about the technology
 export const printIntro = false;
+
 // Is a graphical display needed
 export const needsDisplay = true;
+
 // Executable full path (Required)
-export const cmd = "/sbin/init";
+// Start X directly instead of normal init
+export const cmd = "/usr/bin/startx";
+
 // Arguments, as an array (Required)
-export const args = [];
+// Launch Firefox immediately in kiosk mode
+export const args = [
+	"/usr/bin/firefox",
+	"--no-remote",
+	"--kiosk",
+	"https://example.com"
+];
+
 // Optional extra parameters
 export const opts = {
 	// User id
